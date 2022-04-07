a4249f9a0e24b83b6c1a2504d909e2a87c05b6b7
Exon Reyes <exon1704@gmail.com>
2022-04-07 01:45:44 -0500

    Servicio ToggleSidebar
    Se añade servicio para control de toggle en sidebar

---
+++ src/app/shared/services/toggle-sidebar.service.ts
@@ -0,0 +1,24 @@
+import {EventEmitter, Injectable, Output} from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ToggleSidebarService {
+
+    @Output() change: EventEmitter<boolean> = new EventEmitter();
+    private _isOpen: boolean = false;
+
+    get isOpen(): boolean {
+        return this._isOpen;
+    }
+
+    toggle() {
+        this._isOpen = !this._isOpen;
+        this.change.emit(this._isOpen);
+    }
+
+    update(state: boolean) {
+        this._isOpen = state;
+        this.change.emit(this.isOpen);
+    }
+}
