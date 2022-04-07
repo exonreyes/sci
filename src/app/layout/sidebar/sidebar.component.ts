8b2c4f3cf08047db2ad2f1b1a7eaac0d21e4810e
Exon Reyes <exon1704@gmail.com>
2022-04-07 03:55:52 -0500

    Estructura Sidebar - Skeleton
    Se agregaron módulos con carga dinámicas

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faAngleDown, faBoxesPacking, faChalkboardUser, faChartLine, faHome} from "@fortawesome/free-solid-svg-icons";
+import {faBell, faEnvelope} from "@fortawesome/free-regular-svg-icons";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+    selector: 'app-sidebar', templateUrl: './sidebar.component.html', styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+    angleDown = faAngleDown;
+    bell = faBell;
+    envelope = faEnvelope;
 
-  constructor() { }
+    modules = [{
+        name: "Ventas", icon: "line-chart"
+    }, {
+        name: "Inventario", icon: "stock"
+    }, {
+        name: "Recursos Humanos", icon: "humanresource"
+    }, {name: "Empresa", icon: "business"}];
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
 
 }
--- src/app/layout/skeleton/skeleton.component.ts
+++ src/app/layout/skeleton/skeleton.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ToggleSidebarService} from "@shared/services/toggle-sidebar.service";
 
 @Component({
   selector: 'app-skeleton',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skeleton.component.css']
 })
 export class SkeletonComponent implements OnInit {
+  @ViewChild('sideTogglet') title!: ElementRef;
+  private _toggled = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private toggleSidebarService: ToggleSidebarService) {
   }
 
+  ngOnInit(): void {
+    this.toggleSidebarService.change.subscribe(isOpen => this._toggled = isOpen);
+  }
+
+  onToggle() {
+    this.toggleSidebarService.toggle();
+  }
+
+  // Evento disparado al hacer touch/click en el area principal de la pantalla,
+  // como comodidad para minimizar el menu sidebar
+  eventContent() {
+    if (window.innerWidth >= 992) return
+    if (this.title.nativeElement.classList.contains('sidenav-toggled')) {
+      this.toggleSidebarService.toggle();
+    }
+  }
+
+  get toggled(): boolean {
+    return this._toggled;
+  }
 }
