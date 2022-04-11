e3851310d7dc73585c7dd07904a217734ee2afdf
Exon Reyes <exon1704@gmail.com>
2022-04-11 00:45:15 -0500

    Actualizacion:Sidebar
    - Se actualizaron directivas de Sidebar
    - Se agregaron carga de módulos de manera dinámica usando interpolación

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {faAngleDown, faBoxesPacking, faChalkboardUser, faChartLine, faHome} from "@fortawesome/free-solid-svg-icons";
-import {faBell, faEnvelope} from "@fortawesome/free-regular-svg-icons";
+import {faAngleDown} from '@fortawesome/free-solid-svg-icons';
+import {faBell, faEnvelope} from '@fortawesome/free-regular-svg-icons';
+import {AppConstants} from "@data/constants/AppConstants";
 
 @Component({
     selector: 'app-sidebar', templateUrl: './sidebar.component.html', styleUrls: ['./sidebar.component.css']
@@ -9,19 +10,23 @@ export class SidebarComponent implements OnInit {
     angleDown = faAngleDown;
     bell = faBell;
     envelope = faEnvelope;
-
+    urlDashboard = AppConstants.APP_NAME + '/' + AppConstants.DASHBOARD;
+    private icon_path = "assets/icons/module/";
     modules = [{
-        name: "Ventas", icon: "line-chart"
+        name: 'Ventas', icon: this.icon_path + 'sale/launcher.svg', path: 'sales'
     }, {
-        name: "Inventario", icon: "stock"
+        name: 'Inventario', icon: this.icon_path + 'inventory/launcher.svg', path: 'inventory'
     }, {
-        name: "Recursos Humanos", icon: "humanresource"
-    }, {name: "Empresa", icon: "business"}];
+        name: 'Recursos Humanos', icon: this.icon_path + 'humansrc/launcher.svg', path: 'humansrc'
+    }, {
+        name: 'Empresa', icon: this.icon_path + 'business/launcher.svg', path: 'business'
+    }];
 
     constructor() {
     }
 
     ngOnInit(): void {
+
     }
 
 }
