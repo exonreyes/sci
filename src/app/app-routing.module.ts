57be49717c46cef801842c2d1552da79ac6ac570
Exon Reyes <exon1704@gmail.com>
2022-04-17 17:24:47 -0500

    Se actualizan rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [{
+    path: '', redirectTo: 'dashboard', pathMatch: 'full'
+}, {
     path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule)
-}, {path: 'business', loadChildren: () => import('./modules/business/business.module').then(m => m.BusinessModule)},
-    {path: 'humansrc', loadChildren: () => import('./modules/humansrc/humansrc.module').then(m => m.HumansrcModule)},
-    {
-        path: 'inventory',
-        loadChildren: () => import('./modules/inventory/inventory.module').then(m => m.InventoryModule)
-    },
-    {path: 'sales', loadChildren: () => import('./modules/sales/sales.module').then(m => m.SalesModule)}];
+}, {
+    path: 'business', loadChildren: () => import('./modules/business/business.module').then(m => m.BusinessModule)
+}, {
+    path: 'humansrc', loadChildren: () => import('./modules/humansrc/humansrc.module').then(m => m.HumansrcModule)
+}, {
+    path: 'inventory', loadChildren: () => import('./modules/inventory/inventory.module').then(m => m.InventoryModule)
+}, {
+    path: 'sales', loadChildren: () => import('./modules/sales/sales.module').then(m => m.SalesModule)
+}, {
+    path: '**', redirectTo: 'dashboard', pathMatch: 'full'
+}];
 
+/*ng generate module sales --route sales --module app.module*/
 @NgModule({
     imports: [RouterModule.forRoot(routes, {useHash: true})], exports: [RouterModule]
 })
